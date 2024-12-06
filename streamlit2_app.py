9007a93a65101521fd1158d4bc02b1f392c4772c
PasuphatE <6740155322@student.chula.ac.th>
2024-12-06 16:43:35 +0700

    Update streamlit2_app.py

--- streamlit2_app.py
+++ streamlit2_app.py
@@ -1,27 +1,25 @@
 import streamlit as st
-'''
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
 
-a=2+8
-st.write(
-    a
-)
-
-tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
-
-with tab1:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
-with tab2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
-with tab3:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
-'''
+#st.title("🎈 My new app")
+#st.write(
+#    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+#)
+#a=2+8
+#st.write(
+#    a
+#)
+#
+#tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+#
+#with tab1:
+#    st.header("A cat")
+#    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
+#with tab2:
+#    st.header("A dog")
+#    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+#with tab3:
+#    st.header("An owl")
+#    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
 
 st.title("🎈 Welcome to REGEX generator")
 st.write(
